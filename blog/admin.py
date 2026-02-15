d3dc2f7e77f96d6be4bdc25e123ea060e961355e
Sergio Martinelli <ser.r.martinelli@gmail.com>
2026-02-15 11:46:36 -0300

    Registra modelos en admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Autor)
+admin.site.register(models.Categoria)
+admin.site.register(models.Post)
+
